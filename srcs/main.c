d933b10d6e936f2437d68925e3740b61fb06d00d
Pierre Miceli <pmiceli@MacBook-Pro-de-Pierre.local>
2020-01-14 19:01:21 +0100

    add pika and rondom

--- srcs/main.c
+++ srcs/main.c
@@ -1,6 +1,8 @@
 #include "libft.h"
 #include <time.h>
 #define SIZE_FROG 4444
+#define SIZE_PIKA 3838
+#define NB_GIF 2
 
 int main (void)
 {
@@ -15,9 +17,7 @@ int main (void)
 
     srand(time(NULL));   // Init 4 randomness
     int r = rand();     //r is random
-    r = 0;
-    int foo = 0;
-    if (foo ==0) //need to add rondomness with r%x when add more than dancing frog
+    if (r%NB_GIF == 0)
     {
         printf("Here, i got you a dancing frog ! Hope you like it !\n");
         sleep(1);
@@ -52,12 +52,46 @@ int main (void)
             nanosleep(&t, NULL);
             printf("%s", gif);
             printf("\033[44A"); // bring the cursot 44 lines up AKA begining of the gif 
-            //printf("\033[2J"); //the clear the page similar to clear 
-
             nb_img++;
         }
-
     }
+    else if(r%NB_GIF == 1)
+    {
+        printf("Be a little more like Pikachu and take a deep breath\n");
+        sleep(1);
+        char gif[SIZE_PIKA];
+        int yy = 0;
+        while (gif[yy])
+        {
+            gif[yy] = 0;
+            yy++;
+        }
+        int fd = open("samples/pika", O_RDONLY);
 
+        int nb_line = 0;
+        int j;
+        int nb_img = 0;
+
+        while(1) //do to, faire un systeme qui permet lire l'entree standard pour couper le gif avec Q par exemple...
+        {
+            nb_line = 0;
+            j = 0;
+            if (nb_img == 10)
+            {
+                nb_img = 0;
+                close(fd);
+                fd = open("samples/pika", O_RDONLY);
+                ft_bzero(gif, SIZE_PIKA);
+            }
+            read(fd, &gif, SIZE_PIKA);
+            struct timespec t;
+            t.tv_sec = 0;
+            t.tv_nsec = 125000000;
+            nanosleep(&t, NULL);
+            printf("%s", gif);
+            printf("\033[38A"); // bring the cursot 44 lines up AKA begining of the gif 
+            nb_img++;
+        }
+    }
     return 0;
 }
